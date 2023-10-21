89ac4886878c4fd5eab4793c9f755e6518a0345e
Dewit66 <89267723+Dewit66@users.noreply.github.com>
2023-10-21 12:17:45 -0400

    Day 1 from synchronous lesson

---
+++ day1.py
@@ -0,0 +1,149 @@
+Print = print
+Int = int
+""" toPrint = "Hello World!"
+r = 1
+t = 2
+g = -1.5
+print(toPrint, r+t, abs(g) )
+
+name = input("what is your name?\n")
+print("hello", name)
+
+variable  = int(input("what number are you?"))
+variable2 = int(input("what number are you?"))
+
+print(variable * variable2,"this is it") 
+
+t = "I love CS"
+print(len(t))
+
+x = input("who are you?")
+y = input("what are you?")
+print("you are", y + " " + x)
+
+print("free" in "live free")
+
+print("dewick" in "I like dewick")
+print("fruit" not in "apple is a fruit")
+flower="roses"
+print(flower[1])
+print(flower[-4])
+Print("Sandwhiches" not in "I like eating sandwiches")
+
+x = int(input("type any number"))
+if int(x) == 10:
+  print("x = 10")
+else
+  print("wrong number")
+
+limit = 21
+age = int(input("how old are you?"))
+if age >= limit:
+  print("you can drink")
+else:
+  print("you can't drink")
+
+temp = int(input("what is the temperature:"))
+weather = input("Is it snowing?")
+if temp <= 32:
+  print(apporiate message)
+
+year = int(input("enter a year:\n"))
+if (year % 4) == 0 and year % 100 !=0:
+  print("its a leap year")
+else:
+  print("its not a leap year")
+
+number = int(input("Enter a number:"))
+decimal = float(input("Enter a float:"))
+
+if number * decimal > 100:
+  print(number-decimal)
+elif abs(number - decimal) < 5:
+  name = input("please eneter your name:")
+  print("Hello", name)
+elif number % 2 == 0:
+  result = number/decimal
+  print(result)
+else:
+  print("none of the conditions are true")
+
+
+temp = int(input("whats the temp?"))
+weath = input("whats the weather?")
+if weath == "snowy" and temp < 32:
+  print("apporiate message")
+elif  weath == "snowy" or temp < 32:
+   print("apporiate message")
+else:
+  print("something else")
+
+age = int(input("what is your age?\n"))
+
+if age >= 21 and age < 25:
+  print("you are a young adult")
+elif age >= 25:
+  print("you are an adult")
+else:
+  print("you are underage")
+ 
+mySum = 0
+for i in range(6):
+  mySum = mySum + i
+print(mySum)
+
+for i in range(5):
+  print("Hello world")
+
+for i in range(1,101):
+  print(i)
+
+Sum = 1
+for i in range(1,1001):
+  Sum = Sum * i
+print(Sum)
+
+for i in range(0,11,2):
+  print(i)
+
+i = 0
+while i < 5:
+  print("Hello World")
+  i += 1
+
+i = 5
+while i < 16:
+  print(i)
+  i += 1
+
+mySum = 5
+myProduct = 5
+i = 5
+while i < 21:
+  mySum = mySum + i
+  myProduct = myProduct * i
+  print(i)
+  print(mySum)
+  print(myProduct)
+  i += 1
+
+for i in range(101):
+  if
+
+  elif i % 3 == 0:
+    print("Fizz")
+  elif i % 5 == 0
+    print(Buzz)
+
+i = 1 
+while i <= 15:
+  if i % 2 == 1:
+    print(i)
+  i +=1
+
+i = 1
+while i < 16:
+  print(i)
+  i += 2
+"""
+ 
